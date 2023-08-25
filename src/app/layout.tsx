06c47082a6f2a5aedd5d802f717ab7c09caa4441
Sachintha Sampath <sachinsampath42@gmail.com>
2023-08-25 15:55:08 +0530

    chore: Remove default layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,5 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Container, SSRProvider } from "@/components/bootstrap";
-import NavBar from "./NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,16 +17,14 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {/* <div>This div is shared with all pages in the app</div> */}
-        <SSRProvider>
-          <NavBar/>
-          <main>
-            <Container className="py-4">{children}</Container>
-            {/* since this page is passed as children to the layout page, it can be a server components.
-             * when we pass component as a prop, react does not want to know in advance, the content of the page
-             * just reserver an empty spot in the container component for the children
-             */}
-          </main>
-        </SSRProvider>
+
+        <main>
+          {children}
+          {/* since this page is passed as children to the layout page, it can be a server components.
+           * when we pass component as a prop, react does not want to know in advance, the content of the page
+           * just reserver an empty spot in the container component for the children
+           */}
+        </main>
       </body>
     </html>
   );
